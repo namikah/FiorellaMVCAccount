53e6410ebc912bc3ab9a334deb29d3b490d2bf1c
Namik Heydarov <heydarovnamiq@gmail.com>
2022-02-24 00:25:37 +0400

    Add project files.

---
+++ FirstFiorellaMVC/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using FirstFiorellaMVC.Models;
+using FirstFiorellaMVC.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FirstFiorellaMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        private readonly SignInManager<User> _signInManager;
+
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager = null)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerViewModel);
+            }
+
+            var isExistUser = await _userManager.FindByNameAsync(registerViewModel.Username);
+            if(isExistUser == null)
+            {
+                ModelState.AddModelError("Username", "Allready exist username");
+                return View(registerViewModel);
+            }
+
+            var user = new User()
+            {
+                FullName = registerViewModel.Fullname,
+                UserName = registerViewModel.Username,
+                Email = registerViewModel.Email,
+            };
+
+            var result =await _userManager.CreateAsync(user, registerViewModel.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+                return View(registerViewModel);
+            }
+
+            await _signInManager.SignInAsync(user, false);
+
+            //return RedirectToAction("Index", "Home");
+            return RedirectToAction(nameof(Login));
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstFiorellaMVC/Data/IdentityErrorDescription.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FirstFiorellaMVC.Data
+{
+    public class IdentityErrorDescription : IdentityErrorDescriber
+    {
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError()
+            {
+                Code = "Email",
+                Description = "This e-mail already registered",
+            };
+        }
+    }
+}
